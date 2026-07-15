f79c03d884b7669f87132ae2b98ce49bddb6e4df
Macbook <macbook@MacBook-Air-Macbook.local>
2026-07-16 02:47:57 +0300

    Configure static export and GitHub Actions deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // Если вы используете домен *.github.io/colddev, то basePath должен быть "/colddev".
+  // Если у вас привязан свой личный кастомный домен (например, yaroslav-digital.com),
+  // то basePath должен быть пустой строкой "".
+  basePath: process.env.NODE_ENV === "production" ? "/colddev" : "",
 };
 
 export default nextConfig;
